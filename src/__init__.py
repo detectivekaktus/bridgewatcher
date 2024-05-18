306f8870f9936652a2da5614a3981e94d60dae56
Artiom Astashonak <artiomastashonak@gmail.com>
2024-05-18 22:55:58 +0200

    Fixed user input edge cases.

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 from dotenv import load_dotenv
-from enum import Enum
+from typing import Final
 import os
 
-class Servers(Enum):
-    AMERICA = 1
-    EUROPE  = 2
-    ASIA    = 3
+ERROR:   Final   = 0xff1231
+GOLD:    Final   = 0xf5d62a
+SUCCESS: Final   = 0x66f542
 
 load_dotenv()
 DISCORD_TOKEN: str | None = os.getenv("DISCORD_TOKEN")
--- src/commands.py
+++ src/commands.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from . import ERROR, GOLD
 from .client import bot
 from discord import Embed
 from discord.ext.commands import Context
@@ -7,12 +8,23 @@ import requests as req
 
 @bot.command()
 async def gold(context: Context, entries: int = 4) -> None:
+    if entries not in range(1, 26):
+        await context.send(embed=Embed(title=":red_circle: The entries number is invalid!",
+                                       color=ERROR,
+                                       description="The number of entries must be between 1 and 25."))
+        return
+
     try:
         res: req.Response = req.get(f"https://europe.albion-online-data.com/api/v2/stats/gold?count={entries}", timeout=5)
-        embed: Embed = Embed(title=f":coin: Gold prices",
-                             color=0xf5d62a,
-                             description=f"Here you have your {entries} gold prices on the :flag_eu: European server.\n")
+        if not res.ok:
+            await context.send(embed=Embed(title=":red_circle: Your request cannot be processed!",
+                                           color=ERROR,
+                                           description="The input you provided is invalid and cannot be processed correctly by the system. Please, make a resonable request."))
+            return
 
+        embed: Embed = Embed(title=f":coin: Gold prices",
+                             color=GOLD,
+                             description=f"Here you have your {entries} gold prices on the :flag_eu: European server.")
         for entry in res.json():
             embed.add_field(name=f"Price on {datetime.strptime(entry["timestamp"], "%Y-%m-%dT%H:00:00").strftime("%d.%m.%Y %I %p UTC")}",
                             value=f"${entry["price"]}")
@@ -20,5 +32,5 @@ async def gold(context: Context, entries: int = 4) -> None:
         await context.send(embed=embed)
     except req.ReadTimeout:
         await context.send(embed=Embed(title=":red_circle: Request timed out :(",
-                                       color=0xff1231,
+                                       color=ERROR,
                                        description="I couldn't handle your request due to the long time it took to get the response.\n\nTry again later."))
