aab382e98417d2c4a1b203e7f24059530acc59de
Artiom Astashonak <artiomastashonak@gmail.com>
2026-07-05 22:53:32 +0200

    fix: call logging config

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from bridgewatcher.db.seed.run import seed_if_needed_sync
+from bridgewatcher.loggers import load_logging_config
 
 
 def main() -> None:
     seed_if_needed_sync()
+
+    load_logging_config()
     print("Starting over")
 
 
