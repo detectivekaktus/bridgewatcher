bfa2a53453052d57a88a456d6cde581bae7e02d9
Artiom Astashonak <artiomastashonak@gmail.com>
2026-07-06 11:57:47 +0200

    feat: add market helper api
    The `MarketHelper` class is a base class for all calculation-related
    classes in the future. It's capable of determing the highest and lowest
    prices for items with different query parameters specified in
    `MarketQuery` class.

---
+++ src/bridgewatcher/calc/__init__.py
@@ -0,0 +1,3 @@
+from .market_helper import MarketHelper, MarketQuery
+
+__all__ = ("MarketHelper", "MarketQuery")
---
+++ src/bridgewatcher/calc/market_helper.py
@@ -0,0 +1,102 @@
+from dataclasses import dataclass
+
+from bridgewatcher.api import AlbionOnline
+from bridgewatcher.api.model import Cities, CityPrice, Qualities
+from bridgewatcher.db.schema import Item
+
+
+@dataclass
+class MarketQuery:
+    item_or_id: Item | str
+    quality: Qualities
+    include_black_market: bool
+
+    @staticmethod
+    def with_black_market_included(
+        item_or_id: Item | str, quality: Qualities
+    ) -> "MarketQuery":
+        return MarketQuery(item_or_id, quality, True)
+
+    @staticmethod
+    def with_black_market_excluded(
+        item_or_id: Item | str, quality: Qualities
+    ) -> "MarketQuery":
+        return MarketQuery(item_or_id, quality, False)
+
+
+class MarketHelper:
+    def __init__(self, albion: AlbionOnline) -> None:
+        self.albion = albion
+
+    def _get_prices_for_quality(
+        self,
+        prices: list[CityPrice],
+        quality: Qualities,
+        include_black_market: bool = True,
+    ) -> list[CityPrice]:
+        return [
+            price
+            for price in prices
+            if price.quality == quality.value
+            and (include_black_market or price.city.lower() != Cities.BLACK_MARKET)
+        ]
+
+    async def get_cheapest_item_buy_price(self, query: MarketQuery) -> CityPrice:
+        prices = await self.albion.get_item_prices(query.item_or_id)
+        return self._find_cheapest_buy_price(
+            prices, query.quality, query.include_black_market
+        )
+
+    def _find_cheapest_buy_price(
+        self,
+        prices: list[CityPrice],
+        quality: Qualities,
+        include_black_market: bool = True,
+    ) -> CityPrice:
+        filtered = self._get_prices_for_quality(prices, quality, include_black_market)
+        return min(filtered, key=lambda p: p.buy_price_min)
+
+    async def get_cheapest_item_sell_price(self, query: MarketQuery) -> CityPrice:
+        prices = await self.albion.get_item_prices(query.item_or_id)
+        return self._find_cheapest_sell_price(
+            prices, query.quality, query.include_black_market
+        )
+
+    def _find_cheapest_sell_price(
+        self,
+        prices: list[CityPrice],
+        quality: Qualities,
+        include_black_market: bool = True,
+    ) -> CityPrice:
+        filtered = self._get_prices_for_quality(prices, quality, include_black_market)
+        return min(filtered, key=lambda p: p.sell_price_min)
+
+    async def get_expensive_item_buy_price(self, query: MarketQuery) -> CityPrice:
+        prices = await self.albion.get_item_prices(query.item_or_id)
+        return self._find_expensive_buy_price(
+            prices, query.quality, query.include_black_market
+        )
+
+    def _find_expensive_buy_price(
+        self,
+        prices: list[CityPrice],
+        quality: Qualities,
+        include_black_market: bool = True,
+    ) -> CityPrice:
+        filtered = self._get_prices_for_quality(prices, quality, include_black_market)
+        return max(filtered, key=lambda p: p.buy_price_max)
+
+    async def get_expensive_item_sell_price(self, query: MarketQuery) -> CityPrice:
+        prices = await self.albion.get_item_prices(query.item_or_id)
+        return self._find_expensive_sell_price(
+            prices, query.quality, query.include_black_market
+        )
+
+    def _find_expensive_sell_price(
+        self,
+        prices: list[CityPrice],
+        quality: Qualities,
+        include_black_market: bool = True,
+    ) -> CityPrice:
+        filtered = self._get_prices_for_quality(prices, quality, include_black_market)
+        return max(filtered, key=lambda p: p.sell_price_max)
